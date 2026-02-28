d32430146c6a66dc57fbbcb55f48f522f75e2862
jltamayo2018 <jl.tamayo.2018@alumnos.urjc.es>
2026-02-28 12:47:19 +0100

    arreglo import

--- verdadoreto_app/forms.py
+++ verdadoreto_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Pack, Action, PackCollaborator
-from django.contrib.auth.models import User, UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class PackForm(forms.ModelForm):
     LEVEL_CHOICES = [
