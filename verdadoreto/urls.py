715aee1c62a2411a20754cfa51aa2a22948fe505
jltamayo2018 <jl.tamayo.2018@alumnos.urjc.es>
2026-02-28 13:54:23 +0100

    urls 3

--- verdadoreto/urls.py
+++ verdadoreto/urls.py
@@ -26,15 +26,5 @@ handler500 = "verdadoreto_app.views.handler500"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('verdadoreto_app.urls')),
-
-    path(
-        'accounts/password_reset/',
-        auth_views.PasswordResetView.as_view(
-            template_name='registration/password_reset_form.html',
-            email_template_name='registration/password_reset_email.html',
-        ),
-        name='password_reset'
-    ),
-    
     path('accounts/', include('django.contrib.auth.urls')),
 ]
